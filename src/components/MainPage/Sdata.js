6bc32bf6730112705a6e0d6e80764677a4fc7b15
dolores <loliiipineyro@gmail.com>
2022-12-31 18:24:15 -0300

    commiting

---
+++ src/components/MainPage/Sdata.js
@@ -0,0 +1,27 @@
+const Sdata = [
+    {
+      id: 1,
+      title: "50% Off For Your First Shopping",
+      desc: "Apple Watch 3rd generation",
+      cover: "./images/SlideCard/slide-1.png",
+    },
+    {
+      id: 2,
+      title: "50% Off For Your First Shopping",
+      desc: "Nike Jordan, Red",
+      cover: "./images/SlideCard/slide-2.png",
+    },
+    {
+      id: 3,
+      title: "50% Off For Your First Shopping",
+      desc: "Old Navy, Beige Jacket",
+      cover: "./images/SlideCard/slide-3.png",
+    },
+    {
+      id: 4,
+      title: "50% Off For Your First Shopping",
+      desc: "Pandora Jewlery mistery box",
+      cover: "./images/SlideCard/slide-4.png",
+    },
+  ]
+  export default Sdata
